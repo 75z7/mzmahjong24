f52d189a6a54f944d3d86612dd68de901e7b6aad
75z7 <mz75z7@outlook.jp>
2024-10-24 15:57:11 +0000

    241025

--- main.py
+++ main.py
@@ -46,14 +46,3 @@ def change():
     return render_template(
         "main.html", tehai=tehai, sutehai=sutehai, win=mahjong.judge(tehai)
     )
-
-
-# vue.jsバージョン
-@app.route("/vue")
-def vue():
-    return render_template("vueJong.html")
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
