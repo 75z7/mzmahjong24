6800cb1a6fd23862456f43cbf0dc03146925901d
75z7 <mz75z7@outlook.jp>
2024-10-26 23:29:37 +0000

    241027(仮)

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
 barba.init({
- 
+});
+var $links = $("a[href]");
+var noLinkFunc = function (e) {
+  if (e.currentTarget.href === window.location.href) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+};
+$links.each(function (index, element) {
+  console.log(element);
+  $(element).on("click", function (e) {
+    noLinkFunc(e);
+  });
 });
